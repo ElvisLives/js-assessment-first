137618260e2a5d237a0b6975019075bd8aadf83d
ElvisLives <code@ps.otherinbox.com>
2012-09-24 14:06:29 -0600

    Object tests
    Multiple ways to change context...

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	// obj.sayIt = fn;
+    	// return obj.sayIt()
+    	//fn.call(obj)
+    	return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+    	var properties = [];
+    	for(var property in obj){
+    		if(obj.hasOwnProperty(property))
+    		{
+    			properties.push(property + ": " + obj[property])
+    		}
+    	}
+    	return properties;
     }
   };
 });
