39902e0d7cda19b6bcbd5a75893ad7dc4e8f62e6
ElvisLives <code@ps.otherinbox.com>
2012-09-26 12:19:03 -0600

    Worked on async and functions.
    Struggled with deferreds, first time I have seen them looks like code
    from Tasks.Parallel in .net with continuations.

--- app/async.js
+++ app/async.js
@@ -2,12 +2,50 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
   return {
+  	// examine
     async : function(value) {
+   //  	return {
+			// temp : value,
+			// then : function(result){
+			// 	result(this.temp);
+			// }	
+   //  	}
 
+   var dfd = $.Deferred();
+
+      setTimeout(function() {
+        dfd.resolve(value);
+      }, 1);
+      return dfd.promise();
     },
 
+	// examine
     manipulateRemoteData : function(url) {
+ 		var dfd = $.Deferred();
 
+			$.ajax(url).then(function(resp) {
+				var people = $.map(resp.people, function(person) {
+				  return person.name;
+				});
+				dfd.resolve(people.sort());
+			});
+
+      return dfd.promise();
+
+ //    	var peeps = [];
+ //    	$.ajaxSetup({async:false});
+ //    	$.get(url, function(data){
+	//     		for (var i = data.people.length - 1; i >= 0; i--) {
+	//     			peeps.push(data.people[i]);
+	//     		};
+	//     	});
+	// 	console.log(peeps);
+
+	// return {
+	// 		then : function(){
+	// 			return "datadone";
+	// 		}	
+ //    	}
     }
   };
 });
--- app/functions.js
+++ app/functions.js
@@ -3,35 +3,67 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn(arr[0], arr[1], arr[2]);
     },
 
     speak : function(fn, obj) {
-
+        return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+        var temp = str + ', ';
+        return function(str){
+            temp = temp + str;
+            return temp;
+        }
     },
-
+    // examine
     makeClosures : function(arr, fn) {
+        var functions = [];
 
+        var closedFunc = function(val){
+            return function(){ 
+                return fn(val);
+            };
+        }
+        for (var i = 0; i < arr.length; i++) {
+            // console.log((arr[i]));
+            // functions.push(closedFunc(arr[i]));
+            functions[i] = (closedFunc(arr[i])); // indexing is faster http://www.scottlogic.co.uk/2010/10/javascript-array-performance/
+        };
+        return functions;
     },
 
     partial : function(fn, str1, str2) {
-
+        return function(extra){
+            var args = [str1, str2, extra];
+            return fn.apply(this, args);
+        }
     },
 
     useArguments : function() {
-
+        var sum = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            sum += arguments[i];
+        };
+        return sum;
     },
 
     callIt : function(fn) {
-
+        // have to do this to let us use it as a Javascript array
+        var args = Array.prototype.slice.call(arguments);
+        args.shift(); // remove the first item
+        fn.apply(this,args);
     },
 
+    // examine
     curryIt : function(fn) {
+        var args = Array.prototype.slice.call(arguments, 1, arguments.length);
 
+        return function() {
+            var moreArgs = args.concat(Array.prototype.slice.call(arguments));
+        return fn.apply(null, moreArgs);
+      };
     }
   };
 });
